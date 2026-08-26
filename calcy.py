1745355396eeb3845a3955f08b455ca2e973bc69
gokuls2024e-coder <gokul.s2024e@vitstudent.ac.in>
2026-08-26 15:43:43 +0530

    addition func

---
+++ calcy.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def mul(a,b):
+    return a*b
+def sub(a,b):
+    return a-b
+
+a = 5
+b = 5
+print("  Addition result " , add(a,b))
+
+print("  Subraction result " , sub(a,b))
+
+
